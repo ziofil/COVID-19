95a2408a27abb95ed3d861d66a7bd638afa94dc9
ziofil <miatto@gmail.com>
2020-03-18 12:53:23 +0100

    new version

--- fil.py
+++ fil.py
@@ -2,49 +2,69 @@ import pandas as pd
 import seaborn as sns
 import numpy as np
 from matplotlib import pyplot as plt
-import json5 as js
 
 # LOADING DATA
 
-with open("csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv") as file:
-    confirmed = pd.read_csv(file)
-    confirmed.index.name='Confirmed Cases'
+# with open("csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv") as file:
+#     confirmed = pd.read_csv(file)
+#     confirmed.index.name='Confirmed Cases'
     
-with open("csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Deaths.csv") as file:
-    deaths = pd.read_csv(file)
-    deaths.index.name='Deaths'
+# with open("csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Deaths.csv") as file:
+#     deaths = pd.read_csv(file)
+#     deaths.index.name='Deaths'
     
-with open("csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Recovered.csv") as file:
-    recovered = pd.read_csv(file)
-    recovered.index.name='Recovered'
+# with open("csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Recovered.csv") as file:
+#     recovered = pd.read_csv(file)
+#     recovered.index.name='Recovered'
 
+def select_country(dataframe, country, sum=True):
+    if sum:
+        series = dataframe[dataframe['Country/Region'] == country].sum().drop(['Province/State','Country/Region','Lat','Long'])
+    else:
+        series = dataframe[dataframe['Country/Region'] == country]
 
-def time_plot(dataframe, countries:list, **kwargs):
-    """
-    Utility function to be used with the datasets from CSSE.
-    It automatically computes the delay between countries.
-    """
+    series.index = pd.to_datetime(series.index)
+    series.name = country + f' ({series[-1]} cases)'
+    return series
+
+def compute_lag(ref_series, other_series):
+    '''returns the number of days to shift other_series backward
+    in order to maximize overlap with ref_series'''
+    adj_ref_series = np.pad(np.array(ref_series), (len(ref_series), 0))
+    adj_other_series = np.pad(np.array(other_series), (len(ref_series), 0))
+    penalties = [np.sum(np.abs(adj_ref_series[:-r] - adj_other_series[r:])) for r in range(1, len(ref_series))]
+    delay = np.argmin(penalties) + 1
+    return delay
+
+def time_plot(dataframe, countries:list, lag:int = None, **kwargs):
+    selected_series = [select_country(dataframe, country) for country in countries]
+    order = list(reversed(np.argsort([s[-1] for s in selected_series]))) # order by latest number of cases
     data = pd.DataFrame()
-    series_ref = dataframe[dataframe['Country/Region'] == countries[0]].sum().drop(['Province/State','Country/Region','Lat','Long'])
-    series_ref.index = pd.to_datetime(series_ref.index)
-    series_ref.name = countries[0]
-    data = data.append(series_ref)
-    
-    for c in countries[1:]:
-        series = dataframe[dataframe['Country/Region'] == c].sum().drop(['Province/State','Country/Region','Lat','Long'])
-        delay = np.argmin([np.sum(np.abs(np.array(series_ref[:-r]) - np.array(series[r:]))) for r in range(1, len(series_ref)-10)])+1
-        series.index = pd.to_datetime(series.index)
-        series.index -= pd.Timedelta(f'{delay} day')
-        series.name = c if delay == 0 else c + f' ({delay} days behind)'
-        data = data.append(series)
-    ax = data.transpose().plot(figsize=(10,7), marker='o', title='Time-adjusted confirmed cases', **kwargs)
-    ax.set_ylabel(dataframe.index.name)
+    data = data.append(selected_series[order[0]])
+    for o in order[1:]:
+        series = selected_series[o]
+        lag_ = lag or compute_lag(selected_series[order[0]], series)
+        shifted = series.shift(-lag_)
+        shifted.name = shifted.name + f', {lag_} days behind {countries[order[0]]})'
+        data = data.append(shifted)
+        
+    data = data.transpose()
+    data = data[data.index > '2020-01-23']
+
+    ax = data.plot(figsize=(10,7), linewidth=3.0, grid=True, title = dataframe.index.name, **kwargs)
+    ax.title.set_size(18)
+    ax.tick_params(axis='both', which='major', labelsize=18)
+    ax.tick_params(axis='both', which='minor', labelsize=18)
+    ax.set_ylabel(dataframe.index.name, fontsize=18)
+    ax.legend(prop={'size':16})
+
     return ax
 
 
 class EasyDF(pd.DataFrame):
     
     def __init__(self, df):
+        df.data = pd.to_datetime(df.data).dt.date
         super().__init__(df)
     
     def select_from_column(self, col:str, lst:list):
@@ -65,7 +85,7 @@ class EasyDF(pd.DataFrame):
         self_copy[tmp.columns] = tmp
         return self.__class__(self_copy)
     
-    def plot(self, col:str, regioni:list=None, norm:str=None):
+    def plot(self, col:str, regioni:list=None, norm:str=None, title:str=None, ylabel:str=None):
         if regioni is None:
             hue = None
         else:
@@ -74,19 +94,16 @@ class EasyDF(pd.DataFrame):
             data = self.select_from_column('denominazione_regione', regioni)
         else:
             data = self.renormalize(norm).select_from_column('denominazione_regione', regioni)
-        fig = plt.figure(figsize=(15,3))
+        plt.figure(figsize=(15,5))
         ax = sns.barplot(x="data", y=col, hue=hue, data=data)
         ax.get_xticklabels()
         ax.set_xticklabels(ax.get_xticklabels(), rotation=90)
         plt.legend(loc='best')
-        title = col+'/'+norm if norm else col
+        if title is not None:
+            title = title
+        else:
+            title = col+'/'+norm if norm else col
+        
+        ax.set_ylabel(ylabel or 'Cases')
         ax.set_title(title)
-        return fig
-
-
-
-
-with open("dati-json/dpc-covid19-ita-regioni.json") as file:
-    regioni_raw = js.load(file)
-
-regioni = EasyDF(pd.DataFrame.from_dict(regioni_raw).drop(columns=['stato', 'codice_regione', 'lat', 'long']))
+        return ax
---
+++ generate_plots.py
@@ -0,0 +1,38 @@
+import pandas as pd
+from fil import time_plot, EasyDF
+from datetime import date, timedelta
+
+from os import environ
+environ['TK_SILENCE_DEPRECATION'] = "1"
+
+if __name__ == '__main__':
+
+    print('downloading data...')
+    CONFIRMED_URL = 'https://github.com/CSSEGISandData/COVID-19/raw/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv'
+    confirmed = pd.read_csv(CONFIRMED_URL)
+    confirmed['3/12/20'] = (confirmed['3/13/20']+confirmed['3/11/20'])//2
+    confirmed.index.name = 'Confirmed cases'
+
+    DEATHS_URL = 'https://github.com/CSSEGISandData/COVID-19/raw/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Deaths.csv'
+    deaths = pd.read_csv(DEATHS_URL)
+    deaths['3/12/20'] = (deaths['3/13/20']+deaths['3/11/20'])//2
+    deaths.index.name = 'Deaths'
+
+    REGIONI_URL = 'https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-regioni/dpc-covid19-ita-regioni.csv'
+    regioni = EasyDF(pd.read_csv(REGIONI_URL).drop(columns=['stato', 'codice_regione', 'lat', 'long']))
+
+    print('generating plots...')
+    time_plot(confirmed, ['Italy', 'Spain', 'France', 'Germany', 'United Kingdom']).figure.savefig('plots/europe1_delay.png', bbox_inches='tight')
+    time_plot(confirmed, ['Italy', 'Austria', 'Switzerland', 'Belgium', 'The Netherlands']).figure.savefig('plots/europe2_delay.png', bbox_inches='tight')
+    time_plot(confirmed, ['Italy', 'US', 'Canada']).figure.savefig('plots/it-us-ca_delay.png', bbox_inches='tight')
+    time_plot(confirmed, ['China', 'Italy']).figure.savefig('plots/zh-it_delay.png', bbox_inches='tight')
+    time_plot(deaths, ['Italy', 'Spain', 'France', 'Germany', 'US', 'Canada']).figure.savefig('plots/deaths_delay.png', bbox_inches='tight')
+    time_plot(deaths, ['China', 'Italy']).figure.savefig('plots/deaths_china-vs-italy.png', bbox_inches='tight')
+
+    yesterday = date.today() - timedelta(1)
+    five_most_cases = list(regioni[regioni['data']==yesterday].sort_values(by=['totale_casi']).denominazione_regione)[-5:]
+
+    regioni.plot('totale_casi', five_most_cases, title='Total cases').figure.savefig('plots/IT-cases.png', bbox_inches='tight')
+    regioni.plot('deceduti', five_most_cases, title = 'Total deaths', ylabel='Deaths').figure.savefig('plots/IT-deaths.png', bbox_inches='tight')
+    regioni.plot('deceduti', five_most_cases, norm='totale_casi', title = 'Mortality rate (deaths/tot cases)', ylabel = 'Rate').figure.savefig('plots/IT-mortality.png', bbox_inches='tight')
+    regioni.plot('terapia_intensiva', five_most_cases, norm='totale_ospedalizzati', title = 'ICU rate among hospitalized', ylabel = 'Rate').figure.savefig('plots/IT-ICU_rate.png', bbox_inches='tight')
